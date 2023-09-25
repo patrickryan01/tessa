94f0c81c630f8988e9e390ba44a62fd87e6defa8
Patrick Ryan <pryan@six-industries.com>
2023-09-25 08:19:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ class TESSA:
     def __init__(self):
         self.recognizer = sr.Recognizer()
     
+    def listen(self):
+        """
+        Listen for commands using the microphone and return the recognized text.
+        """
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = self.recognizer.listen(source)
+            try:
+                command = self.recognizer.recognize_google(audio)
+                return command
+            except sr.UnknownValueError:
+                print("Sorry, I didn't catch that.")
+                return None
+
