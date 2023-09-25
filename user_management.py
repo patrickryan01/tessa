e792ac7867a86893b9dea1be909b3294936ca0d9
Patrick Ryan <pryan@six-industries.com>
2023-09-25 08:34:38 -0700

    Update user_management.py

--- user_management.py
+++ user_management.py
@@ -37,3 +37,44 @@ class User(Base):
         return cipher.decrypt(ciphertext).decode()
 
 
+Base.metadata.create_all(engine)
+
+
+def create_user(username, data, key):
+    session = Session()
+    new_user = User(username, data, key)
+    session.add(new_user)
+    session.commit()
+    session.close()
+
+
+def get_user_data(username, key):
+    session = Session()
+    fetched_user = session.query(User).filter_by(username=username).first()
+    session.close()
+    if fetched_user:
+        return fetched_user.decrypt_data(key)
+    else:
+        return None
+
+# For Flask integration and setting up TLS
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    username = request.json.get('username')
+    data = request.json.get('data')
+    key = request.json.get('key')  # IMPORTANT: Do not send the key like this in real-world scenarios
+    create_user(username, data, key)
+    return jsonify({"message": "User created successfully!"}), 201
+
+@app.route('/user/<username>', methods=['GET'])
+def get_user(username):
+    key = request.args.get('key')  # IMPORTANT: Do not send the key like this in real-world scenarios
+    data = get_user_data(username, key)
+    return jsonify({"data": data})
+
+if __name__ == "__main__":
+    app.run(ssl_context=('path_to_cert.pem', 'path_to_key.pem'))
