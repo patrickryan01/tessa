48b6a5c9731950aa22c507add2071c1863f8020d
Patrick Ryan <pryan@six-industries.com>
2023-09-29 21:06:20 -0700

    Update conversation_logic.py

--- modules/conversation_logic.py
+++ modules/conversation_logic.py
@@ -29,6 +29,7 @@ def encrypt(raw, key):
 def decrypt(enc, key):
     enc = base64.b64decode(enc)
     iv = enc[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
 
 
 @app.route("/converse", methods=["POST"])
