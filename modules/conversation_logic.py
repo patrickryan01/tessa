5441747cdd062600a06e07a9a4c7d4ec5ed228de
Patrick Ryan <pryan@six-industries.com>
2023-09-29 21:02:54 -0700

    Update conversation_logic.py

--- modules/conversation_logic.py
+++ modules/conversation_logic.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 openai.api_key = "YOUR_OPENAI_API_KEY"
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-
+LOCK_API_URL = "https://example.com/lock"
 
 # Encryption setup
 BS = 16
