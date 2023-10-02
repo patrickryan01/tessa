ec782d1fbd43a3a75ce982a4a80db186386dbd3d
Patrick Ryan <pryan@six-industries.com>
2023-10-01 20:38:04 -0700

    Update MainActivity.kt

--- tessa_tablet/app/src/main/java/com/example/tessa_tablet/MainActivity.kt
+++ tessa_tablet/app/src/main/java/com/example/tessa_tablet/MainActivity.kt
@@ -96,3 +96,4 @@ class MainActivity : AppCompatActivity(), RecognitionListener, TextToSpeech.OnIn
 
     override fun onResults(results: Bundle?) {
         val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+        if (matches != null) {
